0cdbb9c0974334e19eba81aad0defd686356721d
Aymeric <aymeric.gioia@hotmail.fr>
2017-06-26 15:21:02 +0200

    ajout des fonctionnalité mettre un mot, puis l'afficher dans une phrase

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
-"use strict";
+'use strict';
 
-console.log("fezegzrh");
+document.getElementById('clickMe').addEventListener('click', function () {
+	var Nom = { name: document.getElementById("content").value
+	};
+	document.getElementById('receiver').innerHTML = 'Bonjour ' + Nom.name;
+	if (document.getElementById('content').value == '') {
+		document.getElementById('receiver').innerHTML = 'Bonjour Père Noël';
+	}
+});
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,3 +1,8 @@
-"use strict";
-
-console.log("fezegzrh");
+document.getElementById('clickMe').addEventListener('click',() =>{
+	let Nom = {name: document.getElementById("content").value
+};
+document.getElementById('receiver').innerHTML = `Bonjour ${Nom.name}`;
+if(document.getElementById('content').value == ''){
+	document.getElementById('receiver').innerHTML = 'Bonjour Père Noël';
+}
+});
